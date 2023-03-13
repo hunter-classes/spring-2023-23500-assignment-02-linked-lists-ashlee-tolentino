d92ff607d5bad486a75b5268b11915aed215a3de
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2023-03-13 01:29:41 -0400

    added insert and reverse prototypes

--- OList.h
+++ OList.h
@@ -10,34 +10,13 @@ class OList{
   OList(); // constructor
   ~OList(); // destructor
   
-  void insert(std::string data);
+  void insert(std::string value);
   void insert(int loc, std::string data); // 0 indexed
   int length();
 
   std::string toString(); // for testing purposes
   bool contains(std::string value);
   std::string get(int loc);
-  
   void remove(int loc);
-
-  /*
-    Try to complete for Monday:
-    
-    get()
-    and think about the remove and 
-    other insert routine
-    
-    -------------------
-    ~List();
-    
-    void remove(int loc);
-
-    std::string get(int loc);
-    bool contains();
-    int length(); // the # of elements
-
-  */
-  
-  
-
+  void reverse();
 };
