7a1e5aa21b6f063bdf78442e3f26c3488777c9f9
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2023-03-13 01:22:36 -0400

    added necessary examples

--- main.cpp
+++ main.cpp
@@ -1,34 +1,40 @@
 #include <iostream>
-#include "List.h"
+#include <string>
+#include "OList.h"
+#include "Node.h"
 
 int main()
 {
-  List *l = new List();
-  std::cout << l->toString() << " " << l->length() << "\n";
-  l->insert(0,"a");
-  std::cout << l->toString() << " " << l->length() << "\n";
-  l->insert(0,"b");
-  l->insert(0,"c");
-  l->insert(0,"d");
-  try {
-    l->insert(10,"x");
-  } catch (std::exception e){
-    std::cout << "Insert x didn't work\n";
-  }
-  std::cout << l->toString() << "\n";
-  l->insert(2,"inserted at 2");
-  std::cout << l->toString() << "\n";
-  l->insert(5,"inserted at end");
-  std::cout << l->toString() << " " << l->length() << "\n";
+  OList *l = new OList();
 
-  // New functions for project 2
+  std::cout << l->toString() << " " << l->length() << "\n"; // empty
+    
+  l->insert("b");
+  std::cout << l->toString() << " " << l->length() << "\n"; // b
+  l->insert("d");
+  std::cout << l->toString() << " " << l->length() << "\n"; // b d
+  l->insert("e");
+  std::cout << l->toString() << " " << l->length() << "\n"; // b d e
+  l->insert("a");
+  std::cout << l->toString() << " " << l->length() << "\n"; // a b d e
+  l->insert("c");
+  std::cout << l->toString() << " " << l->length() << "\n"; // a b c d e
+  l->insert("c");
+  std::cout << l->toString() << " " << l->length() << "\n"; // a b c c d e
+  
   std::cout << "----------------------------------------\n";
   std::cout << l->contains("a") << std::endl; // 1
   std::cout << l->contains("d") << std::endl; // 1
-  std::cout << l->contains("inserted at 2") << std::endl; // 1
+  std::cout << l->contains("c") << std::endl; // 1
   std::cout << l->contains("g") << std::endl; // 0
   std::cout << l->contains("z") << std::endl; // 0
-  
+
+  std::cout << "----------------------------------------\n";
+  std::cout << l->get(6) << std::endl; // Location does not exist
+  std::cout << l->get(7) << std::endl; // Location does not exist
+  std::cout << l->get(0) << std::endl; // a
+  std::cout << l->get(5) << std::endl; // e
+
   std::cout << "----------------------------------------\n";
   std::cout << l->toString() << std::endl;
   try{
@@ -49,9 +55,22 @@ int main()
   std::cout << l->toString() << std::endl;
   //l->remove(0);
   //std::cout << l->toString() << std::endl;
-  
-  std::cout << "----------------------------------------\n";
-  delete l;
 
+  std::cout << "----------------------------------------\n";
+  delete l; // Deleting a,
+
+  std::cout << "----------------------------------------\n";
+  OList *l2 = new OList();
+
+  l2->insert("b");
+  l2->insert("d");
+  l2->insert("a");
+  l2->insert("e");
+  l2->insert("c");
+  std::cout << l2->toString() << " " << l2->length() << "\n"; // a b c d e
+
+  l2->reverse();
+  std::cout << l2->toString() << " " << l2->length() << "\n"; // e d c b a
+  
   return 0;
 }
